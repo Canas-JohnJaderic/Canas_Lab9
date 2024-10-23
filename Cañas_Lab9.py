90a1246e517e8a63f9342862255fafc668b4d00a
Canas-JohnJaderic <jayjaycanas06@gmail.com>
2024-10-23 21:07:20 +0800

    Add files via upload

---
+++ Cañas_Lab9.py
@@ -0,0 +1,8 @@
+rows=int(input("Enter Amount of Rows: "))
+base=int(0)
+
+for rows in range(1,+ rows + 1):
+    for rows in range(1,+ rows + 1):
+        base += 1
+        print( base , end=" ")
+    print()
